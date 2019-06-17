8e2e4ea326eddc0d028d1e5a2b1c6987c40cbc25
Sam Johnston <samuelmattjohnston@gmail.com>
2019-06-17 16:32:23 -0500

    The way defaults work make no sense. I got RPC Port working, but I don't get why this works but the HTTPHost doesn't

--- cmd/geth/replicacmd.go
+++ cmd/geth/replicacmd.go
@@ -104,7 +104,7 @@ system and acts as an RPC node based on the replicated data.
 	nodeConfig = node.Config{
 		DataDir:          node.DefaultDataDir(),
 		// HTTPHost:         "0.0.0.0",
-		// HTTPPort:         node.DefaultHTTPPort,
+		HTTPPort:         node.DefaultHTTPPort,
 		HTTPModules:      []string{"net", "web3", "replica"},
 		HTTPVirtualHosts: []string{"*"},
 		WSPort:           node.DefaultWSPort,
