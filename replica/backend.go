57d60d1e1eb3f998ff390bbbd2d5e2cc10f7b502
Austin Roberts <code@ausiv.com>
2020-05-28 15:40:13 -0500

    replica: Add removed flag for removed logs

--- replica/backend.go
+++ replica/backend.go
@@ -465,9 +465,6 @@ func (backend *ReplicaBackend) handleBlockUpdates() {
     if err != nil {
       log.Warn("Error finding common ancestor", "head", headHash, "old", lastBlock.Hash(), "err", err.Error())
     }
-    if len(revertedBlocks) > 0 {
-      log.Info("Handling reorg between %#x and %#x", lastBlock, headBlock)
-    }
     for _, block := range revertedBlocks {
       logs, err := backend.GetLogs(context.Background(), block.Hash())
       if err != nil {
@@ -475,7 +472,10 @@ func (backend *ReplicaBackend) handleBlockUpdates() {
       }
       allLogs := []*types.Log{}
       for _, deletedLogs := range logs {
-        allLogs = append(allLogs, deletedLogs...)
+        for _, deletedLog := range deletedLogs {
+          deletedLog.Removed = true
+          allLogs = append(allLogs, deletedLog)
+        }
       }
       if len(allLogs) > 0 {
         backend.removedLogsFeed.Send(core.RemovedLogsEvent{allLogs})
