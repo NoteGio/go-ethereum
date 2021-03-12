bcd101dcf2e3db7b8d9cfe5bbe8e0b30578d8240
Austin Roberts <austin.roberts@openrelay.xyz>
2021-03-12 14:04:08 -0600

    Exclude precompiles from returned access lists

--- core/state/replica_statedb.go
+++ core/state/replica_statedb.go
@@ -1,31 +1,44 @@
-package state
+	package state
 
-import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-)
+	import (
+		"github.com/ethereum/go-ethereum/common"
+		"github.com/ethereum/go-ethereum/log"
+		"github.com/ethereum/go-ethereum/core/types"
+		"github.com/ethereum/go-ethereum/core/vm"
+	)
+
+var precompiles map[common.Address]struct{}
 
 
 func (s *StateDB) AccessList() *types.AccessList {
-	al := make(types.AccessList, len(s.accessList.addresses))
-	k := 0
+	if precompiles == nil {
+		precompiles = make(map[common.Address]struct{})
+		for _, address := range vm.PrecompiledAddressesBerlin {
+			precompiles[address] = struct{}{}
+		}
+	}
+	al := types.AccessList{}
+	log.Info("Calculating access list")
 	for address, idx := range s.accessList.addresses {
-		var keyCount int
+		if _, ok := precompiles[address]; ok { continue }
+		log.Info("Adding entries", "address", address, "idx", idx)
 		if idx == -1 {
-			keyCount = 0
-		} else {
-			keyCount = len(s.accessList.slots[idx])
+			// al = append(al, types.AccessTuple{
+			// 	Address: address,
+			// 	StorageKeys: []common.Hash{},
+			// })
+			continue
 		}
-		al[k] = types.AccessTuple{
+		k := len(al)
+		al = append(al, types.AccessTuple{
 			Address: address,
-			StorageKeys: make([]common.Hash, keyCount),
-		}
+			StorageKeys: make([]common.Hash, len(s.accessList.slots[idx])),
+		})
 		i := 0
 		for hash := range s.accessList.slots[idx] {
 			al[k].StorageKeys[i] = hash
 			i++
 		}
-		k++
 	}
 	return &al
 }
